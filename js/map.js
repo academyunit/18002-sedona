92d7d1f7bdb2016b4042681e6d78c9b4ea40581f
k <admin@localhost>
2017-10-23 17:55:32 -0700

    Fix header, fix photo.html (add picture polyfill) (B27)

--- js/map.js
+++ js/map.js
@@ -6,8 +6,13 @@
         lng: -111.758749
     };
 
+    var mapIconExtension = 'svg';
+    if (isMisterIE()) {
+        mapIconExtension = 'png';
+    }
+
     var markerData = {
-        markerImg: "../img/icon-map-marker.svg",
+        markerImg: "../img/icon-map-marker." + mapIconExtension,
         markerCoordinates: {
             lat: 34.817941,
             lng: -111.758749
@@ -32,8 +37,31 @@
         new google.maps.Marker({
             position: markerData.markerCoordinates,
             map: map,
+            optimized: false,
             icon: markerData.markerImg
         });
+    }
+
+    function isMisterIE() {
+        var ua = window.navigator.userAgent;
+
+        // IE 10
+        // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+
+        // IE 11
+        // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+
+        // Edge 12 (Spartan)
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+
+        // Edge 13
+        // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+        return (
+            (ua.indexOf('MSIE ') > 0)
+            || (ua.indexOf('Trident/') > 0)
+            || (ua.indexOf('Edge/') > 0)
+        );
     }
 
     window.map = initMap;
